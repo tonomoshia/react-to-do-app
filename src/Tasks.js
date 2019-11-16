03abb24b6b305601ed904776e393f849ce0c8736
Tonomoshia <tonomoshia@users.noreply.github.com>
2019-11-16 13:09:06 -0500

    allow user to mark task as completed

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
 export default class Tasks extends Component {
+  toggleTask = event => {
+    var task = event.target.parentElement;
+    task.classList.add("complete");
+  }
   render() {
     return (
       <ul>
         {this.props.items.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+          {item}
+          <button id="completed" onClick={this.toggleTask}>
+            Completed
+          </button>
+          </li>
         ))}
       </ul>
     );
